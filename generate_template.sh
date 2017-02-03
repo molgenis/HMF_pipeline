093600349865b793f7fa5613e23e7e38b1f64b52
BenjaminsM <m.benjamins@umcg.nl>
2017-02-03 08:26:34 +0100

    updated generate_template

--- generate_template.sh
+++ generate_template.sh
@@ -3,9 +3,10 @@
 module load Molgenis-Compute/v16.11.1-Java-1.8.0_74
 module list
 
-PROJECT=XXX
+PROJECT=XX
 RUNNUMBER=XX
 WORKDIR=/groups/umcg-gaf/tmp04/
+GSDIR=${WORKDIR}/generatedscripts/
 GITHUBDIR=/home/umcg-mbenjamins/github/HMF_pipeline/
 WORKFLOW=${GITHUBDIR}/workflow.csv
 
@@ -16,27 +17,17 @@ then
      rm .compute.properties
 fi
 
-mkdir -p ${WORKDIR}/generatedscripts/
-
-if [ -f ${WORKDIR}/generatedscripts/parameters_converted.csv  ];
+if [ -f ${GSDIR}/parameters_converted.csv  ];
 then
-        rm -rf ${WORKDIR}/generatedscripts/parameters_converted.csv
-fi
-
-if [ -f ${WORKDIR}/generatedscripts/${PROJECT}_converted.csv  ];
-then
-    rm -rf ${WORKDIR}/generatedscripts/${PROJECT}_converted.csv
+        rm -rf ${GSDIR}/parameters_converted.csv
 fi
 
 perl ${GITHUBDIR}/convertParametersGitToMolgenis.pl ${GITHUBDIR}/parameters.csv > \
-${WORKDIR}/generatedscripts/parameters_converted.csv
-
-#perl ${GITHUBDIR}/convertParametersGitToMolgenis.pl ${WORKDIR}/${PROJECT}.csv > \
-#${WORKDIR}/generatedscripts/${PROJECT}_converted.csv
+${GSDIR}/parameters_converted.csv
 
 sh $EBROOTMOLGENISMINCOMPUTE/molgenis_compute.sh \
--p ${WORKDIR}/generatedscripts/parameters_converted.csv \
--p ${WORKDIR}/generatedscripts/${PROJECT}/${PROJECT}.csv \
+-p ${GSDIR}/parameters_converted.csv \
+-p ${GSDIR}/${PROJECT}/${PROJECT}.csv \
 -w ${WORKFLOW} \
 -rundir ${WORKDIR}/projects/${PROJECT}/run${RUNNUMBER}/jobs \
 -b slurm \
